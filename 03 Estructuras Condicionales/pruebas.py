5ac375611d159aa4f23220fd109efe8c30ae4104
idominguez <ivan_dominguez2016@hotmail.com>
2025-03-31 00:59:03 -0300

    Ejericio 9 resuelto

--- 03 Estructuras Condicionales/pruebas.py
+++ 03 Estructuras Condicionales/pruebas.py
@@ -1,6 +1,8 @@
-contraseña = input("Ingrese una contraseña: ")
-largo = len(contraseña)
-if largo >= 8 and largo <= 14:
-    print("Ha ingresado una contraseña correcta")
-else:
-    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+nombre = input("Ingrese su nombre: ")
+opcion = input("Segun las siguientes opciones: \n 1.Si quiere su nombre en mayúsculas. \n 2.Si quiere su nombre en minúsculas \n 3.Si quiere su nombre con la primera letra mayúsculas. \n Ingrese la opcion que desea: ")
+if opcion == "1":
+    print(nombre.lower())
+elif opcion == "2":
+    print(nombre.upper())
+elif opcion == "3":
+    print(nombre.title())
