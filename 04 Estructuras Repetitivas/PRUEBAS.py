24a53e49e28d8da4c56c11dce99a6bf97cc1aa9a
idominguez <ivan_dominguez2016@hotmail.com>
2025-04-26 17:26:42 -0300

    tp terminado

--- 04 Estructuras Repetitivas/PRUEBAS.py
+++
@@ -1,9 +0,0 @@
-#9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe poder procesar 100 números cambiando solo un valor).
-
-suma = 0 #inicializamos la suma
-limite=5
-for i in range (limite):
-    numero = int(input("Ingrese un numero: "))
-    suma+=numero
-
-print("La media es", (suma/limite))
