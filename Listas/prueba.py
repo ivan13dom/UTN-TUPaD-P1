8eb784ee6281c9e026ac44f9d601aa95fa68d304
idominguez <ivan_dominguez2016@hotmail.com>
2025-04-29 22:47:37 -0300

    ejercicio 5

---
+++ Listas/prueba.py
@@ -0,0 +1,6 @@
+#5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
+
+numeros = [8,15,3,22,7]         #En primer lugar se crea la lista con los elementos 8, 15, 3, 22 y 7
+numeros.remove(max(numeros))    #En esta linea se borra el valor maximo de la lista. En este caso 22
+print(numeros)                  #Se imprime la lista como quedo despues de la modificacion
+
